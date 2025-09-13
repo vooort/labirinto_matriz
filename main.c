a4d7b6f7e1be02fd79e427c5051ddf769e0e6fb2
enzo guerra <130326651+vooort@users.noreply.github.com>
2025-09-13 09:10:19 -0300

    Heurística main.c
    Adicionei um contador quantos movimentos faltam para chegar ao destino com a distância de Manhattan.

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 #define tam 5
 
@@ -50,9 +52,15 @@ void matriz(int v[2], char col[tam][tam])
     }
 }
 
+//função para determinar a heurística com a formula de manhattan
+int manhattan(int v[2],int posf[2])
+{
+    return abs(v[0] - posf[0]) + abs(v[1] - posf[1]);
+}
+
 int main()
 {
-    int v[2] = {0,0}, v_ant[2] = {0,0};
+    int v[2] = {0,0}, v_ant[2] = {0,0}, posf[2] = {3,4};
     char dir;
     char col[tam][tam] = {
         {'.','.','.','#','.'},
@@ -71,6 +79,8 @@ int main()
             break;
         }
         printf("Qual direção você deseja ir? use wasd\n"); //requisita a direção com base no wasd do teclado
+        printf("O valor da heurística atual é: %d\n",manhattan(v,posf)); //imprime o valor da heurística com a função manhattan
+
         scanf(" %c",&dir);
         
         v_ant[0] = v[0];  //armazena a posição atual de S 
@@ -84,7 +94,6 @@ int main()
             v[0] = v_ant[0];
             v[1] = v_ant[1];
         }
-        
     }
     return 0;
 }
